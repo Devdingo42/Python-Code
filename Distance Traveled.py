99c1923280a82fc624069c953c2df6fb6f6c3fb3
Devdingo42 <devonduplessie@gmail.com>
2023-09-01 10:05:08 -0700

    a

---
+++ Distance Traveled.py
@@ -0,0 +1,22 @@
+#Distance Traveled
+#Assuming there are no accidents or delays, the distance that a car travels down the interstate can be calculated with the following formula:
+#Distance= Speed x Time
+#A car is traveling at 70 miles per hour. Write a program that displays the following:
+#The distance the car will travel in 6 hours
+#The distance the car will travel in 10 hours
+#The distance the car will travel in 15 hours
+
+#Set speed constant
+speed = 70
+
+#New time 6 hours
+distance = speed * 6
+print('The distance is', distance, 'miles')
+
+#new time 10 hours
+distance = speed * 10
+print('The distance is', distance, 'miles')
+
+#new time 15 hours
+distance = speed * 15
+print('The distance is', distance, 'miles')
---
+++ Personal Information.py
@@ -0,0 +1,11 @@
+#Personal Information
+#Write a program that displays the following information:
+#Your name
+#Your address, with city, state, and ZIP
+#Your telephone number
+#Your college major
+
+print("My name is Devon Duplessie")
+print('I live at 11506 Chestnut Ridge St, Moorpark, CA, 93021')
+print('My telephone number is 805-624-9758')
+print('My college major is software egineering')
